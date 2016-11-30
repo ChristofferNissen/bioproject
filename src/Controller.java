da8ea563288773aa1c9cf6a77d0d0009416906a9
Christoffer Nissen <christoffer.nissen@gmail.com>
2016-11-30 12:23:08 +0100

    Test

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,20 @@
+import Models.Seat;
+
 /**
  * Created by cn on 30/11/2016.
  */
 public class Controller {
+
+    private String name;
+    private  int tal;
+
+    public static void main (String[] args){
+
+        Seat seat = new Seat(0);
+
+    }
+
+
 }
+
+
---
+++ src/Models/Seat.java
@@ -0,0 +1,13 @@
+package Models;
+
+/**
+ * Created by cn on 30/11/2016.
+ */
+public class Seat {
+
+    private int seatNumber;
+
+    public Seat(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+}
