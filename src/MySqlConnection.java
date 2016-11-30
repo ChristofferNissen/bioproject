34fb06de3d7ad3f8f654c9098df9a56eb8565018
Christoffer Nissen <christoffer.nissen@gmail.com>
2016-11-30 16:29:56 +0100

    Added dbcreation.txt & MySqlConnection

---
+++ src/MySqlConnection.java
@@ -0,0 +1,12 @@
+import Models.Seat;
+import java.sql.*;
+
+/**
+ * Created by cn on 30/11/2016.
+ */
+public class MySqlConnection {
+    private static final String MYDB = "ccpbioDB";
+    private static final String USER = "ccpbio";
+    private static final String PASS = "password";
+
+}
