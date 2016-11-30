1b39dfecbf5ddbe2f3809f84822d8084f32ac22f
Cæcilie Iversen <caecilie.iversen@gmail.com>
2016-11-30 17:22:19 +0100

    Jeg er stadig igang med Cinema klassen, og den compiler ikke, bare fyi

--- src/Cinema.java
+++ src/Cinema.java
@@ -6,10 +6,11 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
 
-public class Cinema extends JComponent {
+public class Cinema extends JComponent implements MouseListener {
 
     private int[] rows;
     private int[] seats;
+    private Rectangle rect;
 
     public Cinema()
     {
@@ -17,7 +18,6 @@ public class Cinema extends JComponent {
         seats = new int[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
 
         makeFrame(this);
-
     }
 
     public void makeFrame(Cinema c){
@@ -28,6 +28,11 @@ public class Cinema extends JComponent {
         frame.setVisible(true);
     }
 
+    public void drawSeat()
+    {
+        
+    }
+
     public void paint(Graphics g)
     {
         int width = 20;
@@ -35,11 +40,51 @@ public class Cinema extends JComponent {
         int xValue = 30;
         int yValue = 30;
 
-        for (int i = 1; i <= seats.length; i++) {
-            for (int j = 1; j <= rows.length; j++) {
-                g.drawRect(i*xValue, j*yValue, width, height);
+        for (int i = 0; i <= seats.length; i++) {
+            for (int j = 0; j <= rows.length; j++) {
+                drawSeat();
+                this.addMouseListener(this);
             }
         }
 
+        /*
+        int width = 20;
+        int height = 20;
+        int xValue = 30;
+        int yValue = 30;
+
+        for (int i = 1; i <= seats.length; i++) {
+            for (int j = 1; j <= rows.length; j++) {
+                g.drawRect(i * xValue, j * yValue, width, height);
+                this.addMouseListener(this);
+            }
+        }
+        */
+    }
+
+
+
+    public void mouseClicked(MouseEvent e) {
+        if (rect.contains(e.getX(), e.getY())) {
+            System.out.print("HEJ");
+            repaint();
+        }
+    }
+
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    public void mousePressed(MouseEvent e) {
+
     }
 }
+
