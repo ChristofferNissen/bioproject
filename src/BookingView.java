793bbbee2bd4c576c5961a228b3b8ee92d856569
Christoffer Nissen <christoffer.nissen@gmail.com>
2016-12-15 19:16:57 +0100

    no message

--- src/BookingView.java
+++ src/BookingView.java
@@ -124,7 +124,7 @@ class BookingView {
                     int i = showList.getSelectedIndex();                      //get index of selection
                     int a = (Integer) listModel.get(i).getKey();              //get key for showing
                     Controller.storeShowID(a);                                //store key in Controller
-                    Controller.CreateCinemaViewByShowID(a, "",false);     //get the show with chosen id
+                    Controller.CreateCinemaViewByShowID(a, "",false);         //get the show with chosen id
                 }
         );
 
